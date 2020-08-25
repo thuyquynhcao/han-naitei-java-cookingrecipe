8ec4fa1f5bc27bbfeef7d2f57a88ebcf53ff8cb8
NPLinhUET <lumerijirinmo@gmail.com>
2020-08-25 15:39:27 +0700

    init models & relationships

--- cookingrecipe/src/main/java/com/cookingrecipe/entity/Comment.java
+++ cookingrecipe/src/main/java/com/cookingrecipe/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.cookingrecipe.entity;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,5 +34,9 @@ public class Comment {
 	@NotNull
 	@ManyToOne
 	private Recipe recipe;
-
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	private Date updatedAt;
 }
--- cookingrecipe/src/main/java/com/cookingrecipe/entity/Recipe.java
+++ cookingrecipe/src/main/java/com/cookingrecipe/entity/Recipe.java
@@ -1,8 +1,8 @@
 package com.cookingrecipe.entity;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,9 +14,9 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.format.annotation.NumberFormat;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -59,5 +59,10 @@ public class Recipe {
 
 	@ManyToMany
 	private List<Category> categories;
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	private Date updatedAt;
 
 }
