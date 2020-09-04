8da191c50c4b6f4c8bb1eae11ea5ef3cf40d439b
pre_intern_2020_01_anhnp <phuonganhbs1312@gmail.com>
2020-09-04 08:50:52 +0700

    view login form

--- cookingrecipe/src/main/java/com/cookingrecipe/controller/BaseController.java
+++ cookingrecipe/src/main/java/com/cookingrecipe/controller/BaseController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BaseController {
 	
-	@GetMapping(value = "/home")
+	@GetMapping(value = "/")
 	public String index() {
 		return "home";
 	}
